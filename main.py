a2e93f73d4878b69c4fed0ece03d7a67926fb7e5
vladislavumbrella-web <vladislav.umbrella@gmail.com>
2026-04-19 17:33:36 +0300

    fregwaaa_birthday_bot

--- main.py
+++ main.py
@@ -5,31 +5,29 @@ from aiogram import Bot, Dispatcher
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from handlers.routes import router, check_daily_birthdays, init_db
 
+
 load_dotenv()
+
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def main():
     await init_db()
-
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
-    
     dp.include_router(router)
 
     scheduler = AsyncIOScheduler(timezone="Europe/Kyiv")
-    
+
     scheduler.add_job(
         check_daily_birthdays,
         trigger='cron',
         hour=9,
         minute=0,
-        args=[bot] 
+        args=[bot]
     )
 
     scheduler.start()
-
-    print('Бот успішно запущений через SQLAlchemy.')
-    
+    print('Бот успішно запущений.')
     try:
         await dp.start_polling(bot)
     finally:
